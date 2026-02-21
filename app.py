7d0782824589cedc6489db84264c870f09d31d93
sdpro1234 <sdprooffice1@gmail.com>
2026-02-21 22:30:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,5 +181,8 @@ def logout():
 # ---------------- Run ----------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
